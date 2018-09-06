da437499933b82e9555fc7a5181c1254b31f39af
Évelyne Lachance <eslachance@gmail.com>
2018-09-06 09:27:49 -0400

    Adding missing files

---
+++ commands/ping.js
@@ -0,0 +1,4 @@
+exports.run = async (client, message, args) => {
+  const msg = await message.channel.send("Ping?");
+  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
