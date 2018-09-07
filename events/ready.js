df45da084fa779cf487978fc816b37db627093a7
Évelyne Lachance <eslachance@gmail.com>
2018-09-07 02:30:30 -0400

    Typo fix.

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 module.exports = (client) => {
-  console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
+  console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 }
